c177bc94b44f7567bb87a00aa3965a8c524ced40
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-01 14:11:57 +0300

    design: implement hero area

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1 class="title">
-    We want to make you stronger
-  </h1>
+  <section
+    class="w-full mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl"
+  >
+    <div class="px-6 py-16 md:text-center">
+      <h1
+        class="text-4xl font-semibold leading-tight tracking-tight text-black md:text-5xl"
+      >
+        Stay focused on getting <span class="text-green-dark">stronger</span>
+      </h1>
+
+      <p class="mx-auto mt-5 text-lg md:text-xl text-gray-darker">
+        We're making Mylo to automatically update and manage your workouts.
+        <br />
+        Currently in beta, launching soon.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
