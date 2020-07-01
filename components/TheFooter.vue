3df71364f17318e1f7aecaef286f2be6ec163cde
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-01 15:11:10 +0300

    design: add a footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer
+    class="w-full px-6 py-16 text-lg text-white shadow-inner space-y-5 divide-y bg-green-darker"
+  >
+    <p href="/" class="flex items-center space-x-3">
+      <TheLogo :size="56" />
+      <span>Mylo, your personal training assistant.</span>
+    </p>
+
+    <p class="pt-5 opacity-50">
+      © 2020 Mylo
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="antialiased">
+  <div class="space-y-16 antialiased">
     <TheHeader />
-
     <Nuxt />
+    <TheFooter />
   </div>
 </template>
