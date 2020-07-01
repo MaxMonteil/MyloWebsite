0514d42ffa6e69e9eb716d6c69b84f5c89f430b2
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-01 18:28:21 +0300

    design: keep footer content width in line with header

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,17 @@
 <template>
-  <footer
-    class="w-full px-6 py-16 text-lg text-white shadow-inner space-y-5 divide-y bg-green-darker"
-  >
-    <p href="/" class="flex items-center space-x-3">
-      <TheLogo :size="56" />
-      <span>Mylo is your personal training assistant.</span>
-    </p>
+  <footer class="w-full px-6 py-16 shadow-inner bg-green-darker">
+    <section
+      class="mx-auto text-lg text-white sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl space-y-5 divide-y"
+    >
+      <p href="/" class="flex items-center space-x-3">
+        <TheLogo :size="56" />
+        <span>Mylo is your personal training assistant.</span>
+      </p>
 
-    <p class="pt-5 opacity-50">
-      © 2020 Mylo
-    </p>
+      <p class="pt-5 opacity-50">
+        © 2020 Mylo
+      </p>
+    </section>
   </footer>
 </template>
 
