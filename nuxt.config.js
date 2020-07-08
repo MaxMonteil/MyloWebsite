425dc3713eb64af1d76c4766f5d4ec2c5a77dcc1
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-08 11:35:45 +0300

    build: remove js preloading

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,4 +82,10 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+
+  render: {
+    bundleRenderer: {
+      shouldPreload: (_, type) => type !== 'script',
+    },
+  },
 }
