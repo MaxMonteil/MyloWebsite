cb425d82d0dc7a8b509b7d18bf73e66c492b604c
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-08 11:35:22 +0300

    build: set lazyload as client only plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['@/plugins/vue-lazyload'],
+  plugins: [{ src: '~/plugins/vue-lazyload', mode: 'client' }],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
