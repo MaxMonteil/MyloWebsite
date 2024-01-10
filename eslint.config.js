ffa2f1075962a5b19b3d73a1e0870d02bfe61c8c
Maximilien Monteil <maximilienmonteil@gmail.com>
2024-01-10 12:06:19 +0100

    chore: eslint config formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,22 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    vue: true,
-    typescript: true,
-    overrides: {
-      vue: {
-        'vue/block-order': [
-          'error',
-          {
-            order: ['template', 'script', 'style'],
-          },
-        ],
-        'vue/max-attributes-per-line': ['error', {
-          singleline: { max: 1 },
-          multiline: { max: 1 },
-        }],
-        // this complains about reusable transition components with a slot
-        'vue/require-toggle-inside-transition': 'off',
-      },
+export default antfu({
+  vue: true,
+  typescript: true,
+  overrides: {
+    vue: {
+      'vue/block-order': [
+        'error',
+        {
+          order: ['template', 'script', 'style'],
+        },
+      ],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: { max: 1 },
+        multiline: { max: 1 },
+      }],
+      // this complains about reusable transition components with a slot
+      'vue/require-toggle-inside-transition': 'off',
     },
   },
-)
+})
